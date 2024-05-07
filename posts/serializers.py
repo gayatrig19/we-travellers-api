576512a0455bac99aee9844693f781e46a60b3ed
Gayatri Ghogare <ghogaregayatri@gmail.com>
2024-05-07 21:32:48 +0000

    Add image validation for user uploaded images in Post serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -6,7 +6,27 @@ class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
-    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    profile_image = serializers.ReadOnlyField(
+        source='owner.profile.image.url')
+
+    def validate_image(self, value):
+        """
+        Validate user uploaded images for
+        size, height and width dimensions.
+        """
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError(
+                'Image size larger than 2MB!'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px!'
+            )
+        return value
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -17,5 +37,6 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image', 'place', 'region'
+            'title', 'content', 'image', 'place',
+            'region', 'image_filter'
         ]
