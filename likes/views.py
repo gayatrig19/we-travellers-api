6858f058e0276157a5bde317f7c966c4572ef70e
Gayatri Ghogare <ghogaregayatri@gmail.com>
2024-05-09 15:23:14 +0000

    Add Likelist and LikeDetail generic views and urlpatterns

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from we_travellers_api.permissions import IsOwnerOrReadOnly
+from .models import Like
+from .serializers import LikeSerializer
 
-# Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    """
+    List likes or create a like if user is
+    logged in.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a like or delete it by id if a user
+    owns it.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
