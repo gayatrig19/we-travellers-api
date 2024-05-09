cc9181a28e17067763444a491d3fd24e97c25133
Gayatri Ghogare <ghogaregayatri@gmail.com>
2024-05-09 14:55:38 +0000

    Add create function to LikeSerializer to handle duplicate likes

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from rest_framework import serializers
 from .models import Like
 
@@ -5,10 +6,26 @@ from .models import Like
 class LikeSerializer(serializers.ModelSerializer):
     """
     Serializer for the Like model
-    The create method handles the unique constraint on 'owner' and 'post'
+    The create method handles the unique constraint on
+    'owner' and 'post'
     """
     owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Like
         fields = ['id', 'created_at', 'owner', 'post']
+
+    def create(self, validated_data):
+        """
+        Allows user to create a like.
+        Returns a complete object instance if data is valid.
+        A method to prevent users from creating duplicate likes.
+        Interity error is raised if user likes the
+        same post twice.
+        """
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
