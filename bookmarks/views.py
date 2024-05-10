a94e07067c4f0c1734aac0aa09599b2168d39d1b
Gayatri Ghogare <ghogaregayatri@gmail.com>
2024-05-10 11:23:50 +0000

    Add Bookmark list and detail generic views and urlpatterns

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from we_travellers_api.permissions import IsOwnerOrReadOnly
+from .models import Bookmark
+from .serializers import BookmarkSerializer
 
-# Create your views here.
+
+class BookmarkList(generics.ListCreateAPIView):
+    """
+    List all Bookmarks.
+    Enable user to create a bookmark
+    (bookmark posts) if user is logged in.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = BookmarkSerializer
+    queryset = Bookmark.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class BookmarkDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a Bookmark or delete(remove)
+    it by id if a user owns it.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = BookmarkSerializer
+    queryset = Bookmark.objects.all()
