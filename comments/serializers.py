ed2a6e9ad71426332a59def2058dfd2773d3dbed
Gayatri Ghogare <ghogaregayatri@gmail.com>
2024-05-13 19:49:12 +0000

    Add commentlikes_id field to Comments and a method to know user has liked a comment

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Comment
-
+from commentlikes.models import CommentLike
 
 class CommentSerializer(serializers.ModelSerializer):
     """
@@ -11,17 +11,31 @@ class CommentSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(
         source='owner.profile.image.url')
+    commentlike_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_commentlike_id(self, obj):
+        """
+        Method to let us know if the current user
+        has already liked a comment.
+        """
+        user = self.context['request'].user
+        if user.is_authenticated:
+            commentlike = CommentLike.objects.filter(
+                owner=user, comment=obj
+            ).first()
+            return commentlike.id if commentlike else None
+        return None
 
     class Meta:
         model = Comment
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'post', 'created_at',
-            'updated_at', 'content'
+            'updated_at', 'content', 'commentlike_id'
         ]
 
 
